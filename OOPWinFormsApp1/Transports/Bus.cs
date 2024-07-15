e098814e2d81d07fa76bdb0b8312ff5c3d843d51
Kimy474 <157235200+Kimy474@users.noreply.github.com>
2024-07-15 19:54:24 -0400

    Add files via upload

---
+++ OOPWinFormsApp1/Transports/Bus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPWinFormsApp1.Transports
+{
+    internal class Bus
+    {
+            public void Sound()
+            {
+                SoundPlayer player = new SoundPlayer(@"c:\claxon.mp3");
+                player.Play();
+            }
+
+        }
+    }
---
+++ OOPWinFormsApp1/Transports/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPWinFormsApp1.Transports
+{
+    internal class Car
+    {
+
+        public void Sound()
+        {
+            SoundPlayer player = new SoundPlayer(@"c:\claxon.mp3");
+            player.Play();
+        }
+
+    }
+}
